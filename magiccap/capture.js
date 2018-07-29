3857f747c0ce1dc9b0513bfb1237319382bb330b
Unknown <jake@gealer.email>
2018-07-29 13:58:23 +0100

    More fixes

--- magiccap/capture.js
+++ magiccap/capture.js
@@ -10,19 +10,21 @@ const fsnextra = require("fs-nextra");
 const fs = require("fs");
 const { clipboard } = require("electron");
 
+let captures = global.captures;
+
 exports = class CaptureHandler {
-    async logUpload(filename, success, url, file_path) {
-        captures.captures.push({
-            filename: filename,
-            success: success,
-            url: url,
-            file_path: file_path
-        })
-        fsnextra.writeJSON(`${require("os").homedir()}/magiccap_captures.json`, captures).catch(async() => {
+	async logUpload(filename, success, url, file_path) {
+		captures.captures.push({
+			filename: filename,
+			success: success,
+			url: url,
+			file_path: file_path,
+		});
+		fsnextra.writeJSON(`${require("os").homedir()}/magiccap_captures.json`, captures).catch(async() => {
 			throw new Error("Could not update the capture logging file.");
 		});
-    }
-    // Logs uploads.
+	}
+	// Logs uploads.
 
 	async createCapture(file_path) {
 		let cap_location;
@@ -77,60 +79,60 @@ exports = class CaptureHandler {
 	}
 	// Makes a nice filename for screen captures.
 
-    async handleScreenshotting(filename) {
+	async handleScreenshotting(filename) {
 		let delete_after = true;
-        let save_path, uploader_type, uploader_file, url, uploader;
-        if(config.save_capture) {
-            save_path = config.save_path + filename;
-        } else {
-            save_path = `${os.tmpdir()}/${filename}`;
-            delete_after = false;
-        }
-        let buffer = this.createCapture(save_path);
-        if (config.upload_capture) {
-            uploader_type = config.uploader_type;
-            uploader_file = `./uploaders/${uploader_type}.js`
-            let lstatres = await fsnextra.lstat(uploader_file).catch(() => new Error("Uploader not found."));
+		let save_path, uploader_type, uploader_file, url, uploader, key;
+		if (config.save_capture) {
+			save_path = config.save_path + filename;
+		} else {
+			save_path = `${os.tmpdir()}/${filename}`;
+			delete_after = false;
+		}
+		let buffer = this.createCapture(save_path);
+		if (config.upload_capture) {
+			uploader_type = config.uploader_type;
+			uploader_file = `./uploaders/${uploader_type}.js`;
+			let lstatres = await fsnextra.lstat(uploader_file).catch(() => new Error("Uploader not found."));
 			if (!lstatres.isFile()) throw new Error("Uploader not found.");
-            uploader = require(uploader_file);
-            for(key in uploader.config_options) {
-                if(!(config[uploader.config_options[key]])) {
-                    throw new Error(
-                        "A required config option is missing."
-                    );
-                }
-            }
-            url = await uploader.upload(buffer);
-        }
-        if(config.clipboard_action) {
-            switch(config.clipboard_action) {
-                case 0: { break; }
-                case 1: {
-                    clipboard.writeBuffer(buffer);
-                    break;
-                }
-                case 2: {
-                    if(!url) {
-                        throw new Error(
-                            "URL not found to put into the clipboard. Do you have uploading on?"
-                        );
-                    }
-                    clipboard.writeText(url);
-                    break;
-                }
-                default: {
-                    throw new Error(
-                        "Unknown clipboard action."
-                    );
-                }
-            }
-        }
-        if (delete_after) {
-            await fsnextra.unlink(uploader_file + ".js").catch(async() => new Error("Could not delete capture."));
-            file_path = null;
-        }
-        this.logUpload(filename, true, url, file_path);
-        return "Image successfully captured.";
-    }
-    // Handle screenshots.
-}
+			uploader = require(uploader_file);
+			for (key in uploader.config_options) {
+				if (!config[uploader.config_options[key]]) {
+					throw new Error(
+						"A required config option is missing."
+					);
+				}
+			}
+			url = await uploader.upload(buffer);
+		}
+		if (config.clipboard_action) {
+			switch (config.clipboard_action) {
+				case 0: { break; }
+				case 1: {
+					clipboard.writeBuffer(buffer);
+					break;
+				}
+				case 2: {
+					if (!url) {
+						throw new Error(
+							"URL not found to put into the clipboard. Do you have uploading on?"
+						);
+					}
+					clipboard.writeText(url);
+					break;
+				}
+				default: {
+					throw new Error(
+						"Unknown clipboard action."
+					);
+				}
+			}
+		}
+		if (delete_after) {
+			await fsnextra.unlink(save_path).catch(async() => new Error("Could not delete capture."));
+			save_path = null;
+		}
+		this.logUpload(filename, true, url, save_path);
+		return "Image successfully captured.";
+	}
+	// Handle screenshots.
+};
