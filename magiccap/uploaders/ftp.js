99b51de31a6ee6b5b7634e6d5a089505f789b469
Unknown <SunburntRock89@gmail.com>
2018-08-04 16:25:48 +0100

    FTP is **finally** done :tada:

--- magiccap/uploaders/ftp.js
+++ magiccap/uploaders/ftp.js
@@ -35,11 +35,6 @@ module.exports = {
 			default: "/",
 			required: true,
 		},
-		"Secure Connection": {
-			value: "ftp_secure",
-			type: "boolean",
-			required: true,
-		},
 		Domain: {
 			value: "ftp_domain",
 			type: "text",
@@ -47,17 +42,21 @@ module.exports = {
 		},
 	},
 	upload: async(buffer, filename) => {
-		const client = new (require("basic-ftp")).Client();
-		await client.access({
+		const client = new (require("promise-ftp"));
+
+		await client.connect({
 			host: config.ftp_hostname,
 			port: config.ftp_port,
 			user: config.ftp_username,
 			password: config.ftp_password,
-			secure: config.ftp_secure,
-		}).catch(() => new Error("Could not connect to FTP. Are your credentials correct?"));
-		await client.ensureDir(config.ftp_directory).catch(() => new Error("FTP directory does not exist. Please create it."));
-		await client.upload(buffer, `${config.ftp_directory}/${filename}.png`);
-		await client.close();
-		return `${config.ftp_domain}/${filename}.png`;
+		});
+
+
+		await client.list(config.ftp_directory).catch(() => new Error("FTP directory does not exist. Please create it."));
+		await client.put(buffer, config.ftp_directory.endsWith("/") ? `${config.ftp_directory}${filename}` : `${config.ftp_directory}/${filename}`);
+		client.end();
+
+
+		return `${config.ftp_domain}/${filename}`;
 	},
 };
