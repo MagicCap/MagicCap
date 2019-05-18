45119430bdb8f427ef871121e8f09a342c2fd06b
Unknown <jake@gealer.email>
2019-05-18 18:03:39 +0100

    Try the system keychain

--- scripts/sign_app.sh
+++ scripts/sign_app.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo $P12_CERT | base64 --decode -o ./private.p12
-security import ./private.p12 -P $P12_PASSWORD
+sudo -E security import ./private.p12 -P $P12_PASSWORD -k /Library/Keychains/System.keychain
 rm ./private.p12
 npm i -g electron-osx-sign
-electron-osx-sign --identity="Developer ID Application: Jake Gealer (S7UG4ZL2KJ)" ./build/MagicCap-darwin-x64/MagicCap.app
+sudo electron-osx-sign --identity="Developer ID Application: Jake Gealer (S7UG4ZL2KJ)" ./build/MagicCap-darwin-x64/MagicCap.app
