bc3c78298a9d4a86e938a1b4717bf6c2c3fde5bb
Unknown <jake@gealer.email>
2019-05-18 18:51:53 +0100

    Try setting the system keychain

--- scripts/sign_app.sh
+++ scripts/sign_app.sh
@@ -4,4 +4,4 @@ sudo -E security import ./private.p12 -P $P12_PASSWORD -k /Library/Keychains/Sys
 rm ./private.p12
 npm i -g electron-osx-sign
 sudo security unlock-keychain -u /Library/Keychains/System.keychain
-sudo electron-osx-sign --identity="Developer ID Application: Jake Gealer (S7UG4ZL2KJ)" ./build/MagicCap-darwin-x64/MagicCap.app
+sudo electron-osx-sign --identity="Developer ID Application: Jake Gealer (S7UG4ZL2KJ)" --keychain=/Library/Keychains/System.keychain ./build/MagicCap-darwin-x64/MagicCap.app
