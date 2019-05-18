6da85144aa89ba512f830b2fd17ccbffa788f94f
Unknown <jake@gealer.email>
2019-05-18 17:44:03 +0100

    Lets just remove the keychain name

--- scripts/sign_app.sh
+++ scripts/sign_app.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo $P12_CERT | base64 --decode -o ./private.p12
-security import ./private.p12 -k "~/Library/Keychains/login.keychain" -P $P12_PASSWORD
+security import ./private.p12 -P $P12_PASSWORD
 rm ./private.p12
 npm i -g electron-osx-sign
 electron-osx-sign --identity="Developer ID Application: Jake Gealer (S7UG4ZL2KJ)" ./build/MagicCap-darwin-x64/MagicCap.app
