44b5d8ac6688fca07b2234a901301dbefc1ca433
Unknown <jake@gealer.email>
2019-05-31 16:19:53 +0100

    Upgraded Chainfetch and switched to tiny-s3-uploader for Amazon S3

--- magiccap/uploaders/s3.js
+++ magiccap/uploaders/s3.js
@@ -2,43 +2,8 @@
 // Copyright (C) Jake Gealer <jake@gealer.email> 2018.
 // Copyright (C) Rhys O'Kane <SunburntRock89@gmail.com> 2018.
 
-const magicImports = require("magicimports")
-const AWS = magicImports("aws-sdk")
-const i18n = require("../i18n")
-
-/**
- * The Promise which gets resolved when the S3 upload is finished.
- * @param {AWS.S3} s3 - The S3 object to use to upload the file.
- * @param {string} bucketName - The bucket name of where you want to upload.
- * @param {string} filename - The filename of the upload.
- * @param {Buffer} buffer - The buffer to upload.
- * @returns The data from the S3 upload.
- */
-function s3Promise(s3, bucketName, filename, buffer) {
-    /**
-     * The function embedded into the Promise.
-     */
-    const s3Function = (resolve, reject) => {
-        /**
-         * The main S3 handler.
-         */
-        const s3Handler = (err, data) => {
-            if (err) {
-                reject(err)
-            } else {
-                resolve(data)
-            }
-        }
-        s3.upload({
-            Key: filename,
-            Body: buffer,
-            ACL: "public-read",
-            Bucket: bucketName,
-            ContentType: "image/png",
-        }, s3Handler)
-    }
-    return new Promise(s3Function)
-}
+const { S3 } = require("tiny-s3-uploader")
+const mime = require("mime-types")
 
 module.exports = {
     name: "S3",
@@ -58,7 +23,7 @@ module.exports = {
             value: "s3_endpoint",
             type: "text",
             required: true,
-            default: "https://s3.eu-west-2.amazonaws.com",
+            default: "s3.eu-west-2.amazonaws.com",
         },
         "Bucket Name": {
             value: "s3_bucket_name",
@@ -71,20 +36,16 @@ module.exports = {
             required: true,
         },
     },
-    upload: async(buffer, _, filename) => {
-        AWS.config.update({
-            accessKeyId: config.s3_access_key_id.trim(),
-            secretAccessKey: config.s3_secret_access_key.trim(),
-        })
-        const s3 = new AWS.S3({
-            endpoint: new AWS.Endpoint(config.s3_endpoint),
-        })
-        try {
-            await s3Promise(s3, config.s3_bucket_name, filename, buffer)
-        } catch (err) {
-            const s3Faili18n = await i18n.getPoPhrase("Failed to upload to S3: {err}", "uploaders/exceptions")
-            throw new Error(s3Faili18n.replace("{err}", `${err}`))
+    upload: async(buffer, ext, filename) => {
+        if (config.s3_endpoint.startsWith("http://")) {
+            config.s3_endpoint = config.s3_endpoint.trimStart("http://")
+        } else if (config.s3_endpoint.startsWith("https://")) {
+            config.s3_endpoint = config.s3_endpoint.trimStart("https://")
         }
+        const s3 = new S3(config.s3_endpoint, config.s3_access_key_id.trim(), config.s3_secret_access_key.trim(), config.s3_bucket_name)
+        await s3.upload(
+            filename, "public-read", mime.lookup(ext), buffer,
+        )
         let url = config.s3_bucket_url
         if (!url.endsWith("/")) {
             url += "/"
