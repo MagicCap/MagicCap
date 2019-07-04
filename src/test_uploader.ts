bee42842fdc7837470093b965536d313cc160a39
Jake Gealer <jake@gealer.email>
2019-07-04 20:36:07 +0100

    Fixed a bug with testing uploaders

--- src/test_uploader.ts
+++ src/test_uploader.ts
@@ -2,12 +2,13 @@
 import { readFile } from "fs-nextra"
 import * as uuidv4 from "uuid/v4"
 import { get } from "chainfetch"
+import config from "./config"
 
 // Used for testing a uploader.
 export default async(uploader: any): Promise<(string | boolean)[]> => {
     const logo = await readFile(`${__dirname}/icons/magiccap.png`)
     try {
-        const result = await uploader.upload(logo, "png", `${uuidv4()}.png`)
+        const result = await uploader.upload(config, logo, "png", `${uuidv4()}.png`)
         try {
             await get(result)
         } catch (e) {
