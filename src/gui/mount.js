576043de60521a71019c54cec1482c902a9ee959
Jake Gealer <jake@gealer.email>
2019-08-04 17:40:15 +0100

    Add Sentry back in

--- src/gui/mount.js
+++ src/gui/mount.js
@@ -1,4 +1,13 @@
 import Vue from "vue"
 import App from "./templates/app"
+import * as Sentry from "@sentry/electron"
+
+Sentry.init({
+    dsn: "https://968dcfa0651e40ddaa807bbe47b1aa91@sentry.io/1396847",
+})
+
+Sentry.configureScope(scope => {
+    scope.setUser({ id: window.config.o.install_id })
+})
 
 new Vue(App).$mount("#app")
