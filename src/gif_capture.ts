f87d540ba995a7b905ebac604ac62d6d69f49c7a
Jake Gealer <jake@gealer.email>
2019-08-28 22:13:27 +0100

    Change encoding used on macOS to fix GIF capture

--- src/gif_capture.ts
+++ src/gif_capture.ts
@@ -58,6 +58,7 @@ export async function start(fps: number, x: number, y: number, width: number, he
                 height: height,
             },
             screenId: displayInfo.id,
+            videoCodec: "hevc",
         }
         await aperture.startRecording(settings)
         recording = true
