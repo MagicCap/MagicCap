6beef8abc9eab708af6bd8de7f7530ff927782d5
Jake Gealer <jake@gealer.email>
2020-02-03 23:02:22 +0000

    Create the linux build script with Docker

---
+++ linux_build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+docker build -f Dockerfile.build . -t magiccap-builder
+docker run -v $(pwd):/var/magiccap-mount --name magiccap-builder magiccap-builder
+docker rm magiccap-builder
+docker image rm --force magiccap-builder
