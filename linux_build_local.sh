2afd0123e865574d22422a96802f9c1a0f167438
Jake Gealer <jake@gealer.email>
2020-03-22 00:25:55 +0000

    move the linux build to locally on github actions

---
+++ linux_build_local.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+cd config/
+npm i
+npm run build
+cd ..
+go get
+CURRENT_CWD=$(pwd)
+cd ..
+go get github.com/gobuffalo/packr/packr
+cd $CURRENT_CWD
+~/go/bin/packr build
+mv ./MagicCap ./magiccap-linux
