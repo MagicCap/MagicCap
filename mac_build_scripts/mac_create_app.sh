6e82950f3779eca239badfd93a51e656e8e179d0
Jake Gealer <jake@gealer.email>
2020-04-17 01:18:36 +0100

    Update mac_create_app.sh

--- mac_build_scripts/mac_create_app.sh
+++ mac_build_scripts/mac_create_app.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir -p ./MagicCap.app/Contents/MacOS
-mkdir ./MagicCap.app/Contents/Resources
+mkdir -p ./MagicCap.app/Contents/Resources
 cp ./assets/macos/info.plist ./MagicCap.app/Contents/info.plist
 cp ./magiccap-darwin ./MagicCap.app/Contents/MacOS/MagicCap
 cp ./assets/macos/icon.icns ./MagicCap.app/Contents/Resources/icon.icns
